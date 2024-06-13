95e8ccfe088ca2d456f003f0e2ec9a3a91645191
maadesh <151980884+Maadesh11@users.noreply.github.com>
2024-06-13 12:57:32 +0530

    Create numberstring.js

---
+++ numberstring.js
@@ -0,0 +1,7 @@
+let a='10';
+let b='20';
+// only the'+'the symbol will act like a concatination in both numbers and strings and remaining alla symbols perform the mathematical operation
+document.write(a+b,"<br>");
+document.write(a-b,"<br>");
+document.write(a*b,"<br>");
+document.write(a/b,"<br>");
